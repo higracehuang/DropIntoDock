d853adad51fdab36f0ad064748268b2cfbca1c08
higracehuang <higracehuang@gmail.com>
2023-11-14 20:36:09 -0600

    Initial Commit

---
+++ DropIntoDock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DropIntoDock
+//
+//  Created by Le Huang on 11/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DropIntoDock/DropIntoDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropIntoDockApp.swift
+//  DropIntoDock
+//
+//  Created by Le Huang on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DropIntoDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
